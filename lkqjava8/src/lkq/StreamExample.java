e2cd873aeaaee8b045134ecfacba708c446bcf0b
lkq911009 <387653241@qq.com>
2017-02-12 21:03:07 +0800

    "new work"

--- lkqjava8/src/lkq/StreamExample.java
+++ lkqjava8/src/lkq/StreamExample.java
@@ -11,6 +11,13 @@ public class StreamExample {
 		List<Integer> myList = new ArrayList<>();
 		for (int i = 0; i < 100; i++)
 			myList.add(i);
+		List<String> ls=new ArrayList<String>();
+		for(int i=0;i<10;i++){
+			ls.add("a "+i);
+		}
+		Stream<String> strStream=ls.stream().filter(p->p.contains("a 0"));
+		strStream.forEach(p->System.out.println(p));
+		
 
 		// sequential stream
 		Stream<Integer> sequentialStream = myList.stream();
@@ -19,11 +26,11 @@ public class StreamExample {
 		Stream<Integer> parallelStream = myList.parallelStream();
 
 		// using lambda with Stream API, filter example
-		Stream<Integer> highNums = parallelStream.distinct().filter(p->p>=99);
+		Stream<Integer> highNums = parallelStream.distinct().filter(p->p>=99&&p%2==0);
 		// using lambda in forEach
 		highNums.forEach(p -> System.out.println("High Nums parallel=" + p));
 
-		Stream<Integer> highNumsSeq = sequentialStream.filter(p -> p > 90);
+		Stream<Integer> highNumsSeq = sequentialStream.filter(p->p>=90 && p%2==0);
 		highNumsSeq.forEach(p -> System.out
 				.println("High Nums sequential=" + p));
 
