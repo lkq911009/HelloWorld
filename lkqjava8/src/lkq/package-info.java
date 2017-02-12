c30fa201368a2aebd5294a8e1985312769f86a52
lkq911009 <387653241@qq.com>
2017-02-12 16:53:26 +0800

    "lkq"

---
+++ lkqjava8/src/lkq/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author air
+ *
+ */
+package lkq;
---
+++ lkqjava8/src/lkq/test8.java
@@ -0,0 +1,13 @@
+package lkq;
+
+import java.util.Arrays;
+
+public class test8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Arrays.asList( "p", "k", "u","f", "o", "r","k").forEach( e -> System.out.println( e ) );
+
+	}
+
+}
